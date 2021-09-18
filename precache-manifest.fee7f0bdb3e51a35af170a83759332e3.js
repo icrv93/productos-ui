df27b0f58c75286879606762213310ee28ebcb2c
icrv93 <icrv93@gmail.com>
2021-09-18 12:05:52 -0500

    Updates

---
+++ precache-manifest.fee7f0bdb3e51a35af170a83759332e3.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a5242ee25b24f5a2bad74d0a8197cf",
+    "url": "/tareamongo/index.html"
+  },
+  {
+    "revision": "ae1e2d00cda737ad5361",
+    "url": "/tareamongo/static/css/2.16af0791.chunk.css"
+  },
+  {
+    "revision": "7dcbe452be5eec98dfb6",
+    "url": "/tareamongo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ae1e2d00cda737ad5361",
+    "url": "/tareamongo/static/js/2.57d333bc.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "/tareamongo/static/js/2.57d333bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcbe452be5eec98dfb6",
+    "url": "/tareamongo/static/js/main.feea74d0.chunk.js"
+  },
+  {
+    "revision": "69b0aebd1d044cd3aa56",
+    "url": "/tareamongo/static/js/runtime-main.f8a9e642.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/tareamongo/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/tareamongo/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/tareamongo/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/tareamongo/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/tareamongo/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/tareamongo/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/tareamongo/static/media/primeicons.b0f5d02f.eot"
+  }
+]);
